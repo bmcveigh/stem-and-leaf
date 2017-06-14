89b7e1f2735292b7bd5e9a965b27e44251e5a36b
Brian McVeigh <bmcveigh@metrostarsystems.com>
2017-06-14 09:32:06 -0400

    Cleaning up the code.

--- stem-leaf.js
+++ stem-leaf.js
@@ -1,39 +1,59 @@
-var StemLeaf = StemLeaf ? StemLeaf : {};
-
 (function($) {
+  var StemLeaf = StemLeaf ? StemLeaf : {};
+
+  StemLeaf.Generator = (function () {
+    var self = this;
+
+    function Generator() {
+      StemLeaf.eventHandler.registerEvents();
+    }
+
+    StemLeaf.eventHandler = {
+      
+      registerEvents: function() {
+        $('.container > .row .plot-input')
+          .on('keyup', function(e) {
+          
+          var self = this;
+          var input = $(self).val();
+          var numbers = input.split(' ');
+          var numbersSorted = numbers.sort();
+          
+          var output = '';
+          
+          var lastDigit = 0;
+          
+          var leafNumbers = [];
+          
+          $.each(numbersSorted, function(key, value) {
+            var numberOfDigits = value.length;
+            var splitIndex = numberOfDigits - 1;
+            
+            var stemOutput = '<td>' + value.substring(0, splitIndex) + '</td>';
+            
+            var currentDigit = value.charAt(splitIndex);
+            
+            if (lastDigit == 0 || currentDigit == lastDigit) {
+              leafNumbers.push(currentDigit);
+            }
+            var leafOutput = leafNumbers.join(' ');
+            output += '<tr>' + stemOutput + '<td>' + leafOutput + '</td></tr>';
+            
+            lastDigit = currentDigit;
+          });
+          
+          $(self).siblings('.plot-results').children('table').html(output);
+        });
+      }
+
+    };
+
+    return Generator;
+  })();
+
   $(document).ready(function() {
-    $('.container > .row .plot-input')
-      .on('keyup', function(e) {
-      
-      var self = this;
-      var input = $(self).val();
-      var numbers = input.split(' ');
-      var numbersSorted = numbers.sort();
-      
-      var output = '';
-      
-      var lastDigit = 0;
-      
-      var leafNumbers = [];
-      
-      $.each(numbersSorted, function(key, value) {
-        var numberOfDigits = value.length;
-        var splitIndex = numberOfDigits - 1;
-        
-        var stemOutput = '<td>' + value.substring(0, splitIndex) + '</td>';
-        
-        var currentDigit = value.charAt(splitIndex);
-        
-        if (lastDigit == 0 || currentDigit == lastDigit) {
-          leafNumbers.push(currentDigit);
-        }
-        var leafOutput = leafNumbers.join(' ');
-        output += '<tr>' + stemOutput + '<td>' + leafOutput + '</td></tr>';
-        
-        lastDigit = currentDigit;
-      });
-      
-      $(self).siblings('.plot-results').children('table').html(output);
+    $('.stem-leaf-app').each(function (i, element) {
+      new StemLeaf.Generator();
     });
-});
+  });
 })(jQuery);
